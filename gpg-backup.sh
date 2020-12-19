4f3586af3b28dd9991548da9ad6779467f2cba8e
Nick J Chalko <nick@chalko.com>
2020-12-18 20:41:57 -0800

    backup is working

--- gpg-backup.sh
+++ gpg-backup.sh
@@ -1,3 +1,9 @@
-/bin/bash
+#!/bin/bash
 
+tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 key=$1
+
+
+echo ${key}
+tar -acf ${key}.tar.gz ~/.gnupg/${key}/*
+gpg --comment "${key}.tar.gz " --enarmor <${key}.tar.gz > ${key}.tar.gz.asc
