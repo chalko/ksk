be13d9f2051412b70e49787bd8b03af9da16f221
Nick J Chalko <nick@chalko.com>
2020-12-30 11:50:59 -0800

    Add signing and auth keys

--- init-ksk.sh
+++ init-ksk.sh
@@ -28,10 +28,17 @@ cp ${DIR}/gpg.conf ~/gpg-primary/
 gpg --homedir ~/gpg-primary/ \
   --quick-generate-key "${NAME} <${EMAIL}>" default
 
-KEY_ID=$(gpg --homedir ~/gpg-primary --with-colons --list-key "${EMAIL}" | grep -m 1 pub | cut -d: -f 5)
-FGPR=$(gpg --homedir ~/gpg-primary --with-colons --list-key "${EMAIL}" | grep -m 1 fpr | cut -d : -f 10)
+KSK_ID=$(gpg --homedir ~/gpg-primary --with-colons --list-key "${EMAIL}" | grep -m 1 pub | cut -d: -f 5)
+KSK_FGPR=$(gpg --homedir ~/gpg-primary --with-colons --list-key "${EMAIL}" | grep -m 1 fpr | cut -d : -f 10)
 
 cat >>~/ksk-secure/ksk.conf <<EOL
-export KSK_ID=0x${KEY_ID}
-export KSK_FGPR=${FGPR}
+export KSK_ID=0x${KSK_ID}
+export KSK_FGPR=${KSK_FGPR}
 EOL
+
+cp ~/gpg-primary/openpgp-revocs.d/${KSK_FGPR}.rev \
+  ~/ksk-secure/${KSK_FGPR}.rev
+
+gpg --homedir ~/gpg-primary --quick-add-key $KSK_FGPR default sign
+gpg --homedir ~/gpg-primary --quick-add-key $KSK_FGPR default auth
+
