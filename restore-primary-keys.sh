64d549968c912d0986960a0bdbd3bcd48bcf1d52
Nick Chalko <nick@chalko.com>
2025-09-12 16:56:09 -0700

    use dev/shm

--- restore-primary-keys.sh
+++ restore-primary-keys.sh
@@ -5,24 +5,27 @@ CWD="$(pwd)"
 
 source ${DIR}/ksk.conf
 
+PRIMARY_DIR=/dev/shm/ksk/gpg-primary
+KSK_SECURE_DIR=/dev/shm/ksk/ksk-secure
+
 if [ -d ~/gpg-primary ]; then
-  echo "ERROR: ~/gpg-primary exists.  you must delete the directory to create a new primary key"
+  echo "ERROR: ${PRIMARY_DIR} exists.  you must delete the directory to create a new primary key"
   exit 1
 fi
 
-mkdir ~/gpg-primary
-chmod 700 ~/gpg-primary
-rm -rf ~/ksk-secure
-mkdir ~/ksk-secure
-chmod 700 ~/ksk-secure
+mkdir -p ${PRIMARY_DIR}
+chmod 700 ${PRIMARY_DIR}
+rm -rf ${KSK_SECURE_DIR}
+mkdir -p ${KSK_SECURE_DIR}
+chmod 700 ${KSK_SECURE_DIR}
 
-git clone ${DIR}/.git ~/ksk-secure
+git clone ${DIR}/.git ${KSK_SECURE_DIR}
 
-cp ~/ksk-secure/gpg.conf ~/gpg-primary
-gpgconf --homedir ~/gpg-primary --kill gpg-agent
-gpg --homedir ~/gpg-primary --batch --import ${DIR}/$KSK_ID.public.gpg-key
-gpg --homedir ~/gpg-primary --batch --import ${DIR}/$KSK_ID.private.gpg-key
-gpg --homedir ~/gpg-primary --import-ownertrust ~/ksk-secure/ownertrust.txt
+cp /${KSK_SECURE_DIR}/gpg.conf ${PRIMARY_DIR}
+gpgconf --homedir ${PRIMARY_DIR} --kill gpg-agent
+gpg --homedir ${PRIMARY_DIR} --batch --import ${DIR}/$KSK_ID.public.gpg-key
+gpg --homedir ${PRIMARY_DIR} --batch --import ${DIR}/$KSK_ID.private.gpg-key
+gpg --homedir ${PRIMARY_DIR} --import-ownertrust ${KSK_SECURE_DIR}/ownertrust.txt
 
 
 
